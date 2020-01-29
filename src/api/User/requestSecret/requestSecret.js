944e93c8e4c482158cfaa50cdb61d7e1dd38ae81
ISPserver <tjdals080@naver.com>
2020-01-29 21:02:00 +0900

    Passport JWT part Three

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -2,7 +2,7 @@ import { generateSecret, sendSecretMail } from "../../../utils";
 import { prisma } from "../../../../generated/prisma-client";
 export default {
     Mutation:{
-        requestSecret: async (_, args) => {
+        requestSecret: async (_, args, {request}) => {
             const { email } = args;
             const loginSecret = generateSecret();            
             try{
--- src/passport.js
+++ src/passport.js
@@ -1,3 +1,5 @@
+//passport는 쿠키와 세션 작업을 하기에 좋다. 
+//쿠키를 가져오고 만들어주는 모든 일을 하기 때문
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config({path: path.resolve(__dirname, ".env") });
@@ -9,11 +11,11 @@ import { prisma } from "../generated/prisma-client";
 const jwtOptions = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.JWT_SECRET
-}
+};
 
 const verifyUser = async(payload, done) => {
     try{
-        const user = await prisma.user({id: payload.id})
+        const user = await prisma.user({id: payload.id});
         if(user !== null){
             return done(null,user);
         } else {
@@ -24,4 +26,17 @@ const verifyUser = async(payload, done) => {
     }    
 };
 
+
+export const authenticatejwt = (req, res, next) => 
+passport.authenticate("jwt",  { sessions:false }, (error,user) => {
+    if(user){
+        req.user = user;
+    }
+    next();
+})(req, res, next);
+//여기까지 과정 => 1.토큰을 받아서 2.해석하고
+//                3.사용자를 찾고 4.사용자가 존재하면 req객체에 사용자 추가
+//                5. graphql 함수를 실행하는 것.
+
 passport.use(new Strategy(jwtOptions, verifyUser));
+passport.initialize();
