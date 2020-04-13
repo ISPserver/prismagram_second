dec9bce40087f8e19dbddd984a6ba6b33c25eedc
ISPserver <tjdals080@naver.com>
2020-04-13 18:44:27 +0900

    seeRoom Resolver

--- src/fragments.js
+++ src/fragments.js
@@ -1,6 +1,7 @@
 export const USER_FRAGMENT = `
     id
-    username    
+    username
+    avatar    
 `;
 
 export const COMMENT_FRAGMENT = `
@@ -15,6 +16,17 @@ export const COMMENT_FRAGMENT = `
 export const FILE_FRAGMENT = `
     id
     url
+`;
+
+export const MESSAGE_FRAGMENT = `
+    id
+    text
+    to {
+        ${USER_FRAGMENT}
+    }
+    from {
+        ${USER_FRAGMENT}
+    }
 `;
 
 export const FULL_POST_FRAGMENT = `
@@ -34,11 +46,16 @@ fragment PostParts on Post{
 }
 `;
 
+
 export const ROOM_FRAGMENT = `
     fragment RoomParts on Room {
         id
         participants {
-            id
+            ${USER_FRAGMENT}
+        }
+        messages {
+            ${MESSAGE_FRAGMENT}
         }
     }
-`
+`;
+
