b75a6a12f3616e3dadd3a9da2893f877a6559f45
ISPserver <tjdals080@naver.com>
2020-04-13 20:04:51 +0900

    newMessage Subscription part one & two

--- src/api/User/confirmSecret/confirmSecret.js
+++ src/api/User/confirmSecret/confirmSecret.js
@@ -10,7 +10,7 @@ export default {
                 await prisma.updateUser({
                     where: { id: user.id },
                     data: {
-                        loginSecre: ""
+                        loginSecret: ""
                     }
                 })
                 return generateToken(user.id);                
