28c0a6e2f30b68566e30e7f4dc6c112aa5573081
ISPserver <tjdals080@naver.com>
2020-04-13 21:58:16 +0900

    Kill The Fragments

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -2,6 +2,11 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
     Post: {
+        //field를 computed field로 다 교체해서 이제 fragment
+        //사용할 필요가 없게끔 한게 7~9줄
+        files: ({ id }) => prisma.post({ id }).files(),
+        comments: ({ id }) => prisma.post({ id }).comments(),
+        user: ({ id }) => prisma.post({ id }).user(),
         isLiked: (parent, _, {request}) => {
             const { user } = request;
             const { id } = parent;
